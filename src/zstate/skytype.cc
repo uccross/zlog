9830d768b36f169d799cc7064d16bbb5700b8183
Noah Watkins <noahwatkins@gmail.com>
2015-11-09 16:14:59 -0800

    zstate: wip
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/zstate/skytype.cc
+++ src/zstate/skytype.cc
@@ -18,6 +18,52 @@ int SkyObject::UpdateHelper(ceph::bufferlist& bl)
   return ret;
 }
 
+/*
+ * Modifications made within a transactional context are saved and later
+ * written as part of the commit record for this transaction.
+ */
+int SkyObject::UpdateHelper(ceph::bufferlist& bl, SkyObject::Transaction& txn)
+{
+  return 0;
+}
+
+/*
+ * Doesn't roll the log foward, but instead saves the object id and version
+ * being read in the transaction object.
+ */
+int SkyObject::QueryHelper(SkyObject::Transaction& txn)
+{
+  return 0;
+}
+
+/*
+ * When playing back the stream we have to be careful with commit records. A
+ * commit record describes a transaction that may involve multiple objects.
+ * Before making a commit/abort decision based on the transaction's read set
+ * the log is played forward for each object involved before checking for read
+ * conflicts.
+ *
+ * Not all clients may host all objects, (from tango paper) "so writes or
+ * reads can involve objects that are not locally hosted at the client that
+ * generates the commit record or the client that encounters it."
+ *
+ * - Remote writes at the generating client
+ *     - Generating client is the client that executed the transaction,
+ *     created the commit record, and appended it to the log.
+ *
+ *     - Remote write is a write to an object that is not hosted by the client
+ *     generating and executing the transaction.
+ *
+ *     - Client does not need to play a stream to append to it (TODO... how?),
+ *     so the generating client can append commit record to stream of remote
+ *     object.
+ *
+ *
+ * - Remote writes at the consuming client
+ *     - This is when a client is playing back a log and encounters a commit
+ *     record that contains a write to an object that it doesn't locally host.
+ *     - Update local objects, ignore updates to objects that it doesn't host.
+ */
 int SkyObject::QueryHelper()
 {
   int ret = stream_->Sync();
@@ -37,6 +83,7 @@ int SkyObject::QueryHelper()
         return 0; // end of stream
 
       case -ENODEV:
+        // ReadNext returns the correct position for this error case
         ret = log_->Fill(pos);
         if (!ret || ret == -EROFS)
           break; // try again
--- src/zstate/skytype.h
+++ src/zstate/skytype.h
@@ -6,6 +6,39 @@
 namespace skytype {
 
 class SkyObject {
+ public:
+  class Transaction {
+    /*
+     * Speculative commit records are produced and appended to the log. A
+     * commit record contains a read set for isolation. The read set is a list
+     * of (object, version) read by the transaction where the version is the
+     * last offset in the shared log that modified the object. A transaction
+     * succeeds if none of its reads are stale (objects unchanged since being
+     * read).
+     *
+     *
+     */
+   public:
+    // appends a commit record, and plays the log forward until the commit
+    // point to determine abort/commit decision.
+    //
+    // read only transactions don't add a commit record but rather just play
+    // the log forward to the current tail. after checking the tail from teh
+    // sequencer, if there is nothing to read, the trasnactions can
+    // commit/abort locally.
+    //
+    // write-only transactions append commit record, but can commit
+    // immediately without playing log forward.
+    //
+    // map.insert(0) always succeeds woudl be an example of write-only xtn. if
+    // we wanted semantics like an error if 0 already existed, then this is
+    // actually a read and a write.
+    //
+    // uses multi-append for write-set
+    //
+    int Submit();
+  };
+
  protected:
   /*
    *
@@ -25,7 +58,17 @@ class SkyObject {
   /*
    *
    */
+  int UpdateHelper(ceph::bufferlist& bl, Transaction& txn);
+
+  /*
+   *
+   */
   int QueryHelper();
+
+  /*
+   *
+   */
+  int QueryHelper(Transaction& txn);
 
  private:
   zlog::Log::Stream *stream_;
